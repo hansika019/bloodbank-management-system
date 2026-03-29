ea27b588102660220e2beda7dc340b4fcae7a481
hansika <hansikaganji@gmail.com>
2026-03-30 02:21:05 +0530

    fix syntax error

--- src/main/java/com/bloodbank/BloodbankApplication.java
+++ src/main/java/com/bloodbank/BloodbankApplication.java
@@ -1,4 +1,4 @@
-package your.package.name; // keep your existing package
+package com.bloodbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
