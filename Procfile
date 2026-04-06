dea741126958ca2c6c3b9db4ddc152ebf86db57e
hansika <hansikaganji@gmail.com>
2026-04-06 22:16:10 +0530

    backend ready for railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/bloodbank-0.0.1-SNAPSHOT.jar
--- src/test/java/com/bloodbank/BloodbankApplicationTests.java
+++ src/test/java/com/bloodbank/BloodbankApplicationTests.java
@@ -1,13 +1,14 @@
 package com.bloodbank;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class BloodbankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
